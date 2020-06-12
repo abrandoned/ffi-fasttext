d87a135220e7c51384e5cec5afb3b13b99bc8ef7
Nephi Allred <nephi.allred@mx.com>
2020-06-11 19:10:51 -0600

    more debug code

--- vendor/fasttext/fasttext.cc
+++ vendor/fasttext/fasttext.cc
@@ -145,9 +145,10 @@ void FastText::loadModel(const std::string& filename) {
 }
 
 void FastText::loadModel(std::istream& in) {
-  if (!checkModel(in)) {
-    return;//throw std::runtime_error("Model file has wrong file format!");
-  }
+  //if (!checkModel(in)) {
+  //  return;//throw std::runtime_error("Model file has wrong file format!");
+  //}
+char bff[8]; in.read(bff,8);
   args_ = std::make_shared<Args>();
   dict_ = std::make_shared<Dictionary>(args_);
   input_ = std::make_shared<Matrix>();
